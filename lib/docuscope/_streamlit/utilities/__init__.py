c09552a3d95e342e282bd2c45c42c1e61a2719b6
browndw <davidwestbrown@gmail.com>
2024-08-06 16:49:29 -0700

    Update __init__.py

--- lib/docuscope/_streamlit/utilities/__init__.py
+++ lib/docuscope/_streamlit/utilities/__init__.py
@@ -1,21 +1,7 @@
-import pathlib
-from importlib.machinery import SourceFileLoader
 
-# set paths
-HERE = pathlib.Path(__file__).parents[1].resolve()
-OPTIONS = str(HERE.joinpath("options.toml"))
-IMPORTS = str(HERE.joinpath("utilities/handlers_imports.py"))
-
-# import options
-_imports = SourceFileLoader("handlers_imports", IMPORTS).load_module()
-_options = _imports.import_options_general(OPTIONS)
-
-if _options['global']['desktop_mode'] == False:
-	from server.downloads import download
-	from server.session import session_state
-
-if _options['global']['desktop_mode'] == True:
+if __package__ == 'docuscope._streamlit.utilities':
 	from ..server.downloads import download
 	from ..server.session import session_state
-
-
+else:
+	from server.downloads import download
+	from server.session import session_state
