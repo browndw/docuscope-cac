7cc5817c13f368d6d8cf2fc3fc99bece7640defe
browndw <davidwestbrown@gmail.com>
2024-08-06 15:54:54 -0700

    Update token_frequencies.py

--- lib/docuscope/_streamlit/apps/token_frequencies.py
+++ lib/docuscope/_streamlit/apps/token_frequencies.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 David West Brown
+# Copyright (C) 2024 David West Brown
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,34 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import base64
-from io import BytesIO
+import pandas as pd
 import pathlib
-from importlib.machinery import SourceFileLoader
+import polars as pl
+import st_aggrid
+import st_aggrid.grid_options_builder
+import streamlit as st
 
-# set paths
-HERE = pathlib.Path(__file__).parents[1].resolve()
-OPTIONS = str(HERE.joinpath("options.toml"))
-IMPORTS = str(HERE.joinpath("utilities/handlers_imports.py"))
-
-# import options
-_imports = SourceFileLoader("handlers_imports", IMPORTS).load_module()
-_options = _imports.import_options_general(OPTIONS)
-
-modules = ['categories', 'handlers', 'messages', 'states', 'warnings', 'streamlit', 'docuscospacy', 'pandas', 'st_aggrid']
-import_params = _imports.import_parameters(_options, modules)
-
-for module in import_params.keys():
-	object_name = module
-	short_name = import_params[module][0]
-	context_module_name = import_params[module][1]
-	if not short_name:
-		short_name = object_name
-	if not context_module_name:
-		globals()[short_name] = __import__(object_name)
-	else:
-		context_module = __import__(context_module_name, fromlist=[object_name])
-		globals()[short_name] = getattr(context_module, object_name)
+from docuscope._streamlit import categories as _categories
+from docuscope._streamlit import states as _states
+from docuscope._streamlit.utilities import handlers_database as _handlers
+from docuscope._streamlit.utilities import messages as _messages
+from docuscope._streamlit.utilities import warnings as _warnings
 
 CATEGORY = _categories.FREQUENCY
 TITLE = "Token Frequencies"
@@ -47,24 +31,41 @@ KEY_SORT = 2
 
 def main():
 
-	session = _handlers.load_session()	
+	user_session = st.runtime.scriptrunner.script_run_context.get_script_run_ctx()
+	user_session_id = user_session.session_id
 
-	if session.get('freq_table') == True:
+	if user_session_id not in st.session_state:
+		st.session_state[user_session_id] = {}
+	try:
+		con = st.session_state[user_session_id]["ibis_conn"]
+	except:
+		con = _handlers.get_db_connection(user_session_id)
+		_handlers.generate_temp(_states.STATES.items(), user_session_id, con)
+
+	try:
+		session = pl.DataFrame.to_dict(con.table("session").to_polars(), as_series=False)
+	except:
+		_handlers.init_session(con)
+		session = pl.DataFrame.to_dict(con.table("session").to_polars(), as_series=False)
 	
-		_handlers.load_widget_state(pathlib.Path(__file__).stem)
-		metadata_target = _handlers.load_metadata('target')
+	if session.get('freq_table')[0] == True:
+	
+		_handlers.load_widget_state(pathlib.Path(__file__).stem, user_session_id)
+		metadata_target = _handlers.load_metadata('target', con)
 						
 		st.sidebar.markdown("### Tagset")
 		
-		tag_radio = st.sidebar.radio("Select tags to display:", ("Parts-of-Speech", "DocuScope"), key = _handlers.persist("ft_radio", pathlib.Path(__file__).stem), horizontal=True)
+		tag_radio = st.sidebar.radio("Select tags to display:", ("Parts-of-Speech", "DocuScope"), key = _handlers.persist("ft_radio", pathlib.Path(__file__).stem, user_session_id), horizontal=True)
 
 		if tag_radio == 'Parts-of-Speech':			
-			df = _handlers.load_table('ft_pos')
+			df = con.table("ft_pos", database="target").to_pyarrow_batches(chunk_size=5000)
+			df = pl.from_arrow(df).to_pandas()
 		if tag_radio == 'DocuScope':			
-			df = _handlers.load_table('ft_ds')
+			df = con.table("ft_ds", database="target").to_pyarrow_batches(chunk_size=5000)
+			df = pl.from_arrow(df).to_pandas()
 	
 		st.markdown(_messages.message_target_info(metadata_target))
-			
+		
 		gb = st_aggrid.GridOptionsBuilder.from_dataframe(df)
 		gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=100) #Add pagination
 		gb.configure_column("Token", filter="agTextColumnFilter", headerCheckboxSelection = True, headerCheckboxSelectionFilteredOnly = True)
@@ -79,8 +80,8 @@ def main():
 			df,
 			gridOptions=go,
 			enable_enterprise_modules = False,
-			data_return_mode='FILTERED_AND_SORTED', 
-			update_mode='MODEL_CHANGED', 
+			data_return_mode='FILTERED_AND_SORTED',
+			update_mode='MODEL_CHANGED',
 			columns_auto_size_mode='FIT_CONTENTS',
 			theme='alpine',
 			height=500, 
@@ -92,48 +93,46 @@ def main():
 			st.markdown(_messages.message_columns_tokens)
 		
 		st.sidebar.markdown("---")
-		
+			
+		selected = grid_response['selected_rows']
+
+		if selected is not None:
+			df = pd.DataFrame(selected)
+			n_selected = len(df.index)
+			st.markdown(f"""##### Selected rows:
+			   
+			Number of selected tokens: {n_selected}
+			""")
+
 		with st.sidebar.expander("Filtering and saving"):
 			st.markdown(_messages.message_filters)
-			
-		selected = grid_response['selected_rows'] 
-		if selected:
-			st.write('Selected rows')
-			df = pd.DataFrame(selected).drop('_selectedRowNodeInfo', axis=1)
-			st.dataframe(df)
 	
-		st.sidebar.markdown(_messages.message_download)
-		if st.sidebar.button("Download"):
-			with st.sidebar:
-				with st.spinner('Creating download link...'):
-					towrite = BytesIO()
-					downloaded_file = df.to_excel(towrite, encoding='utf-8', index=False, header=True)
-					towrite.seek(0)  # reset pointer
-					b64 = base64.b64encode(towrite.read()).decode()  # some strings
-					st.success('Link generated!')
-					linko= f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="token_frequencies.xlsx">Download Excel file</a>'
-					st.markdown(linko, unsafe_allow_html=True)
+		with st.sidebar:
+			st.markdown(_messages.message_download)
+			download_file = _handlers.convert_to_excel(df)
+
+			st.download_button(
+    			label="Download to Excel",
+    			data=download_file,
+    			file_name="token_frequencies.xlsx",
+   					 mime="application/vnd.ms-excel",
+					)
 		st.sidebar.markdown("---")
 				
 	else:
 		st.markdown(_messages.message_tables)
 		st.sidebar.markdown(_messages.message_generate_table)
-	
+
 		if st.sidebar.button("Frequency Table"):
-			if session.get('target_path') == None:
+			if session.get('has_target')[0] == False:
 				st.markdown(_warnings.warning_11, unsafe_allow_html=True)
 			
 			else:
 				with st.sidebar:
 					with st.spinner('Processing frequencies...'):
-						tp = _handlers.load_corpus_session('target', session)
-						metadata_target = _handlers.load_metadata('target')
-						wc_pos = ds.frequency_table(tp, metadata_target.get('words'))
-						wc_ds = ds.frequency_table(tp, metadata_target.get('tokens'), count_by='ds')
-					_handlers.save_table(wc_pos, 'ft_pos')
-					_handlers.save_table(wc_ds, 'ft_ds')
-					_handlers.update_session('freq_table', True)
-					st.experimental_rerun()
+						_handlers.update_session('freq_table', True, con)
+					
+					st.rerun()
 		
 		st.sidebar.markdown("---")
 
