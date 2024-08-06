983122aa5b8d207d823d6d34fa15bcb269f8cd05
browndw <davidwestbrown@gmail.com>
2024-08-06 16:50:18 -0700

    Update __init__.py

--- lib/docuscope/_streamlit/utilities/__init__.py
+++ lib/docuscope/_streamlit/utilities/__init__.py
@@ -1,7 +1,3 @@
 
-if __package__ == 'docuscope._streamlit.utilities':
-	from ..server.downloads import download
-	from ..server.session import session_state
-else:
-	from server.downloads import download
-	from server.session import session_state
+from ..server.downloads import download
+from ..server.session import session_state
